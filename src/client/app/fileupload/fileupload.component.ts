b09991b735aa7b79fbcffc87cb0e4c3a0e8e5255
Antony Baasandorj <antony.baasan@gmail.com>
2017-10-24 21:48:31 -0400

    show upload file list

--- src/client/app/fileupload/fileupload.component.ts
+++ src/client/app/fileupload/fileupload.component.ts
@@ -10,7 +10,9 @@ import { Http } from '@angular/http';
   styleUrls: ['fileupload.component.css'],
 })
 export class FileUploadComponent implements OnInit {
+  public fileList: File[] = [];
   private apiEndPoint: string = 'http://localhost:5000/api/PdfManager/UploadFiles';
+
   // tslint:disable-next-line:member-ordering
   @ViewChild('fileInput') fileInput: any;
   // tslint:disable-next-line:no-empty
@@ -22,11 +24,25 @@ export class FileUploadComponent implements OnInit {
 
   }
 
-  public upload() {
+  public chooseFile() {
     const fileBrowser = this.fileInput.nativeElement;
-    if (fileBrowser.files && fileBrowser.files[0]) {
+    this.fileList.push(fileBrowser.files[0]);
+
+    console.log(this.fileList.length);
+  }
+
+  public upload() {
+    this.SentFileUplaodRequest(this.fileList);
+  }
+
+  private SentFileUplaodRequest(files: File[]) {
+    if (files && files.length > 0) {
       const formData = new FormData();
-      formData.append('files', fileBrowser.files[0]);
+
+      files.forEach((f, i) => {
+        formData.append('file' + i, f);
+      });
+
       const xhr = new XMLHttpRequest();
 
       // let args: RequestOptionsArgs = {};
